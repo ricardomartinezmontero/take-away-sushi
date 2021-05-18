c1e2b19dedd9cd4bfb63895867d4c03b5ccf15c7
Ricardo <ricardo.martinez.montero@gmail.com>
2021-05-18 18:28:27 +0200

    move to nextjs

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -4,19 +4,19 @@ export const menu = [
         items: [
             {
                 name: "Combo 1",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "Gyozas de pollo, tartar de salmón y ensalada de col china",
                 price: 14.90
             },
             {
                 name: "Combo 2",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "Gyozas de pollo, tartar de atún y ensalada de col china",
                 price: 14.90
             },
             {
                 name: "Combo 3",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "Gyozas de pollo,bandeja maki mix (24 piezas) y ensalada wakame",
                 price: 16.90
             }
@@ -27,31 +27,31 @@ export const menu = [
         items: [
             {
                 name: "Bandeja Salmón, 16 Piezas",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "4 sashimi salmón, 4 nigiri salmón y 8 philadelphia roll",
                 price: 16.50
             },
             {
                 name: "Bandeja Atún, 16 Piezas",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "4 sashimi atún, 4 nigiri atún y 8 california atún roll",
                 price: 16.50
             },
             {
                 name: "Bandeja Vegetal, 16 Piezas",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "4 maki aguacate, 4 maki vegetal y 8 mango roll",
                 price: 16.50
             },
             {
                 name: "Bandeja sin Pescado Crudo, 16 Piezas",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "4 pollo roll, 2 nigiri anguila, 2 nigiri langostino, 4 sapporo roll y 4 cangrejo roll",
                 price: 16.50
             },
             {
                 name: "Bandeja Gourmet Mix, 24 Piezas",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "4 green dragon roll, 4 california atún roll, 4 pez mantequilla trufado roll, 4 philadelphia roll, 4 maki atún y 4 maki salmón",
                 price: 16.50
             }
@@ -62,31 +62,31 @@ export const menu = [
         items: [
             {
                 name: "Gyozas de Pollo, 4 Unidades",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "",
                 price: 3.90
             },
             {
                 name: "Ensalada Wakame",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "",
                 price: 4.50
             },
             {
                 name: "Edame",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "",
                 price: 5.50
             },
             {
                 name: "Goyzas de Vegetales, 4 Unidades",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "",
                 price: 3.90
             },
             {
                 name: "Ceviche de Lubina",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "",
                 price: 3.50
             }
@@ -97,25 +97,25 @@ export const menu = [
         items: [
             {
                 name: "Nigiri Salmón, 2 Unidades",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "",
                 price: 3.95
             },
             {
                 name: "Nigiri Salmón Flambeado, 2 Unidades",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "",
                 price: 3.95
             },
             {
                 name: "Nigiri Atún, 2 Unidades",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "",
                 price: 4.50
             },
             {
                 name: "Nigiri Pez Mantequilla, 2 Unidades",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "",
                 price: 4.95
             }
@@ -126,13 +126,13 @@ export const menu = [
         items: [
             {
                 name: "Arroz con Leche de Coco",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "",
                 price: 5.50
             },
             {
                 name: "Mousse de Chocolate",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "",
                 price: 4.50
             }
@@ -143,25 +143,25 @@ export const menu = [
         items: [
             {
                 name: "Coca Cola Normal",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "",
                 price: 2.00
             },
             {
                 name: "Coca Cola Zero",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "",
                 price: 2.00
             },
             {
                 name: "Fanta Naranja Normal",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "",
                 price: 2.00
             },
             {
                 name: "Fanta Naranja Zero",
-                image: process.env.PUBLIC_URL + 'prod1.jpg',
+                image: 'prod1.jpg',
                 description: "",
                 price: 2.00
             }
